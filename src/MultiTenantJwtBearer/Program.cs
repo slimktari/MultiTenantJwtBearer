b0e58eedf64859709eb2d71c91db6c817bd6a813
Slim Ktari <code@slimktari.com>
2024-11-24 22:57:03 +0100

    Remove Swashbuckle package and few cosmetics changes.

--- src/MultiTenantJwtBearer/Program.cs
+++ src/MultiTenantJwtBearer/Program.cs
@@ -1,4 +1,3 @@
-
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using MultiTenantJwtBearer.Contracts;
 using MultiTenantJwtBearer.MultiTenancy;
@@ -18,43 +17,35 @@ namespace MultiTenantJwtBearer
             bool useSecurityTokenValidators = builder.Configuration.GetValue<bool>("Authentication:Schemes:Bearer:UseSecurityTokenValidators");
 
             var mainSchemeHandler = JwtBearerDefaults.AuthenticationScheme;
-            builder.Services.AddAuthentication(
+            builder.Services
+                .AddAuthentication(
                     options =>
                     {
-                        // Setting the main scheme as the default is necessary for the challenge scheme to function correctly.
+                        // Setting the main scheme as the default is necessary for the challenge scheme to work correctly.
                         options.DefaultAuthenticateScheme = mainSchemeHandler;
                         options.DefaultChallengeScheme = mainSchemeHandler;
                         options.DefaultScheme = mainSchemeHandler;
                         options.DefaultForbidScheme = mainSchemeHandler;
                     })
                 .AddMultiTenantJwtBearerToken(mainSchemeHandler, options =>
-            {
-                // Custom options for multi-tenant JWT validation.
-                options.Audience = audience;
-                options.Authority = authority;
-                options.RequireHttpsMetadata = requireHttpsMetadata;
-                options.UseSecurityTokenValidators = useSecurityTokenValidators;
-            });
+                {
+                    // Custom options for multi-tenant JWT validation.
+                    options.Audience = audience;
+                    options.Authority = authority;
+                    options.RequireHttpsMetadata = requireHttpsMetadata;
+                    options.UseSecurityTokenValidators = useSecurityTokenValidators;
+                });
 
             builder.Services.AddControllers();
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
-
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
+            
             app.UseMiddleware<TenantNameMiddleware>();
             app.UseAuthentication();
             app.UseHttpsRedirection();
             app.UseAuthorization();
-            
             app.MapControllers();
-            
             app.Run();
         }
     }
